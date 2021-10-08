359eb2bec791ddfb27025507ccfbdc2c5e28eb67
Deshani <deshani422@gmail.com>
2021-10-08 21:56:28 +0530

    change code 2

--- BAD2.py
+++ BAD2.py
@@ -2,7 +2,7 @@
 
 GPA = float(input("Enter GPA:"))
 if GPA > 2.0:
-    print ("Welcome to our university")
+    print ("Welcome to Moratuwa university")
 
 else:
-    print ("Your application is rejected")
+    print ("Your application is rejected. Try next time")
