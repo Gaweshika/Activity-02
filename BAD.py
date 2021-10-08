d0e797ac1d1f3e49ce3fd3bc54470f165487ef1e
Deshani <deshani422@gmail.com>
2021-10-08 18:51:58 +0530

    insert code

---
+++ BAD.py
@@ -0,0 +1,14 @@
+
+
+
+a = int(input("Enter a :"))
+b = int(input("Enter b :"))
+c = int(input("Enter c :"))
+delta = b*b - 4*c
+if delta < 0:
+    print("complex roots !")
+else:
+    t = math.sqrt(delta)
+    r1 = (-b -t) / (2*a)
+    r2 = (-b +t) / (2*a)
+    print("Roots are: ", r1, r2) # display the roots
